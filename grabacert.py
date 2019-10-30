93bbd2743b55ce896bdbe1c4aa332569a4139d1e
garrettcrosby <55802750+garrettcrosby@users.noreply.github.com>
2019-10-30 08:41:40 -0400

    update 2
    added logging

--- grabacert.py
+++ grabacert.py
@@ -1,12 +1,15 @@
 import requests
 import configparser
+import logging
+import logging.handlers
+import socket
 from datetime import datetime, timedelta
 from subprocess import call
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from os import path
 
-def get_rootCA(vault_server, int_ca):
+def get_rootCA(vault_server, int_ca, cn):
     request_ca = 'https://{0}/v1/pki/ca/pem'.format(vault_server)
     request_int = 'https://{0}/v1/pki/cert/{1}'.format(vault_server, int_ca)
     r = requests.get(request_ca, verify=False)
@@ -22,7 +25,8 @@ def get_rootCA(vault_server, int_ca):
         with open(int_ca_path, 'w') as f:
             f.write(int_ca_txt)
         call('update-ca-trust')
-    except Exception as e: print(e)
+    except:
+        syslog.error('failed to install root or int CA on {0}'.format(cn))
 
 def grab_cert(vault_server, token, cn, ttl):
     request_url = 'https://{0}/v1/pki_int/issue/privatesharp-dot-com'.format(vault_server)
@@ -31,17 +35,24 @@ def grab_cert(vault_server, token, cn, ttl):
     r = requests.post(request_url, headers = {'X-Vault-Token': token}, data=str(data), verify=verify)
     return r.json()
 
-def install_cert(response, cert_path, key_path):
+def install_cert(response, cert_path, key_path, cn):
     data = response['data']
     cert = data['certificate']
     key = data['private_key']
-    with open (cert_path, 'w') as f:
-        f.write(cert)
-    with open (key_path, 'w') as f:
-        f.write(key)
+    try:
+        with open (cert_path, 'w') as f:
+            f.write(cert)
+        with open (key_path, 'w') as f:
+            f.write(key)
+        syslog.info('cert and key installed on {0}'.format(cn))
+    except:
+        syslog.critical('failed to install cert or key on {0}'.format(cn))
 
 def hook(cmd):
-    call(cmd, shell=True)
+    try:
+        call(cmd, shell=True)
+    except:
+        syslog.error('grabacert could not restart services on {0}'.format(cn))
 
 def check_cert(cert_path):
     with open(cert_path, 'r') as f:
@@ -58,7 +69,7 @@ def check_cert(cert_path):
         return False
 
 def main():
-
+    
     #declare variables. These are read from ini file
     Config = configparser.ConfigParser()
     Config.read('config.ini')
@@ -71,10 +82,23 @@ def main():
     ttl = Config.get('config', 'ttl')
     has_root = Config.get('config', 'has_root')
     cmd = Config.get('config', 'cmd')
+    syslog_server = Config.get('config', 'syslog')
+    syslog_port = Config.get('config', 'syslog_port')
 
+    #set up logging to syslog server
+    syslog = logging.getLogger('syslog')
+    syslog.setLevel(logging.DEBUG)
+    syslog_handler = logging.handlers.SyslogHandler(
+                     address=(syslog_server, syslog_port), 
+                     facility=LOG_NEWS,
+                     socktype=socket.SOCK_STREAM)
+    syslog.addHandler(syslog_handler)
+    
     #check for prescence of root_ca, get it if needed
+    syslog.info('grabacert is checking for the rootCA on {0}'.format(cn))
     if has_root != 'True':
-        get_rootCA(vault_server, int_ca)
+        syslog.warning('grabacert is installing rootCA on {0}'.format(cn))
+        get_rootCA(vault_server, int_ca, cn)
         Config.set('config', 'has_root', 'True')
 
     #check validity of cert
@@ -82,15 +106,16 @@ def main():
         if check_cert(cert_path) == False:
         #False means the cert has not passed the threshold for a renewal
         #so we will exit
+            syslog.info('cert is good on {0}'.format(cn))
             exit()
         else:
         #A return of true means that 75% of the cert's validity period has passed
         #Let's go ahead and grab a new one
+            syslog.warning('renewing cert for {0}'.format(cn))
             cert = grab_cert(vault_server, token, cn, ttl)
-            install_cert(cert, cert_path, key_path)
+            install_cert(cert, cert_path, key_path, cn)
             if cmd != "":
                 hook(cmd)
 
 if __name__ == '__main__':
     main()
-        
